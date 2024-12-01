7e470ca5eabd8940048d74bf4505ab96ed81d292
Edi <poqueque@yahoo.es>
2024-12-01 06:40:19 +0100

    Day 1

--- lib/days/day01.dart
+++ lib/days/day01.dart
@@ -6,11 +6,40 @@ class Day01 extends Day {
 
   @override
   part1() {
-    return 0;
+    var sum = 0;
+    var list1 = <int>[];
+    var list2 = <int>[];
+    for (var i = 0; i < inputList.length; i++) {
+      var a = inputList[i].split("   ");
+      list1.add(int.parse(a[0]));
+      list2.add(int.parse(a[1]));
+    }
+    list1.sort();
+    list2.sort();
+    for (var i = 0; i < list1.length; i++) {
+      var d = list1[i]- list2[i];
+      if (d < 0) d = -d;
+      sum += d;
+    }
+    return sum;
   }
-
   @override
   part2() {
-    return 0;
+    var sum = 0;
+    var list1 = <int>[];
+    var list2 = <int>[];
+    for (var i = 0; i < inputList.length; i++) {
+      var a = inputList[i].split("   ");
+      list1.add(int.parse(a[0]));
+      list2.add(int.parse(a[1]));
+    }
+    list1.sort();
+    list2.sort();
+    for (var i = 0; i < list1.length; i++) {
+      var sim = list2.where((element) => element == list1[i]).length;
+        list1[i] *= sim;
+      sum += list1[i];
+    }
+    return sum;
   }
 }
